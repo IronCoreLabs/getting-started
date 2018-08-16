4c3eacd72c6c480401a84828ed81eded95119127
Ernie Turner <ernie.turner@gmail.com>
2018-08-16 08:58:30 -0600

    Bump dependencies to remove various security issues (#13)

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,7 @@ module.exports = (env) => {
                 }));
             },
         },
+        mode: "development",
         entry: [
             'whatwg-fetch',
             "babel-polyfill",
